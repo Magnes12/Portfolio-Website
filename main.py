6a1b9dad751f60df60cbe53b3499ac80fe01d83d
Damian <93340676+Magnes12@users.noreply.github.com>
2024-01-09 12:37:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+# colors 
+    #DCF2F1
+    #7FC7D9
+    #365486
+    #0F1035
